966037bb7066da0a2fabeeb9eec11e2680474a21
schaafie <pieter@jort.net>
2020-12-28 09:18:12 +0100

    update on data-vault

--- target/admin/js/page-content2.js
+++ target/admin/js/page-content2.js
@@ -15,8 +15,13 @@ export default class pageContent {
     test() {
         var dv = new dataVault( this.dataDef )
 
+        console.log( dv.getValue('first' ));
         console.log( dv.getValue('friends/1/last' ));
-
+        dv.addSetItem( 'friends' );
+        dv.addSetItem( 'friends' );
+        console.log( dv );
+        dv.removeSetItem( 'friends/2' );
+        console.log( dv );
     
     }
 
