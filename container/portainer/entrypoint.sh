8944b26069615451ccdcd21af41c140478ed8b09
schaafie <pieter@jort.net>
2025-11-02 14:54:27 +0100

    housekeeping

---
+++ container/portainer/entrypoint.sh
@@ -0,0 +1,26 @@
+# entrypoint.sh
+#!/bin/bash
+
+# Docker entrypoint script.
+# Wait until Postgres is ready
+echo "Testing if Postgres is accepting connections. {$PGHOST} {$PGPORT} ${PGUSER}"
+while ! pg_isready -q -h $PGHOST -p $PGPORT -U $PGUSER; do
+  echo "$(date) - waiting for database to start"
+  sleep 2
+done
+
+# If database is not created, build the new database
+# NOTE: Environment variables starting with PG are crucial for correct connection
+echo ">>>> Checking database "
+if [[ -z $(psql -Atqc "\list $PGDATABASE") ]]; then
+  echo ">>>> Database $PGDATABASE does not exist. Creating..."
+  mix ecto.create
+  mix ecto.migrate
+  mix run priv/repo/seeds.exs
+  echo ">>>> Database $PGDATABASE created."
+else
+  echo ">>>> Database exists."
+fi
+
+echo ">>>> Done."
+exec mix phx.server
